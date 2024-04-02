00727d5068a8f31e46aa702446df17c7775d217b
Emil <eacahila@gmail.com>
2024-04-02 10:36:57 +0800

    AppConfig

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Analyn and Ryan Wedding',
-  title: 'Analyn and Ryan Wedding',
+  site_name: 'Analyn and Ryan',
+  title: 'Analyn and Ryan',
   description: 'Website for the wedding of Analyn and Ryan',
   locale: 'en',
 };
