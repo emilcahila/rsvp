cebd7bf5a28f74cf5f9f1073715122b176e04efc
Emil <eacahila@gmail.com>
2024-04-02 10:22:48 +0800

    default app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,9 @@
-// pages/_app.tsx
-
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
 
-import ErrorBoundary from '@/error/errorBoundary';
-
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ErrorBoundary fallback={<div>Something went wrong.</div>}>
-    <Component {...pageProps} />
-  </ErrorBoundary>
+  <Component {...pageProps} />
 );
 
 export default MyApp;
