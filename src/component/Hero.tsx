e08dce70c22ee37bc0bc6309660dc8da9bc8e97d
Emil <eacahila@gmail.com>
2024-05-21 15:56:50 +0800

    Update font again

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -32,8 +32,8 @@ const DateBox = () => (
 
 const DetailsContainer = () => (
   <div className="font-LibreBaskerville item-center mt-4 justify-center text-center text-white sm:ml-7 sm:mt-8 sm:text-center">
-    <p className="text-base sm:text-lg">invite you to celebrate</p>
-    <p className="text-base sm:text-lg">their love and marriage</p>
+    <p className="sm:text-lg">invite you to celebrate</p>
+    <p className="sm:text-lg">their love and marriage</p>
     <p className="mt-4 text-sm">SATURDAY, 14TH OF DECEMBER</p>
     <p className="text-sm sm:text-base">TWENTY TWENTY-FOUR</p>
     <p className="mt-4 text-sm">six o&apos;clock in the evening</p>
