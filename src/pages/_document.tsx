595eeb1c2daaff151544fbb5c8786b5eea219218
Emil <eacahila@gmail.com>
2024-05-22 01:16:50 +0800

    Move assets and add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,34 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 import { AppConfig } from '../utils/AppConfig';
 
-// Need to create a custom _document because i18n support is not compatible with `next export`.
-class MyDocument extends Document {
-  // eslint-disable-next-line class-methods-use-this
-  render() {
-    return (
-      <Html lang={AppConfig.locale}>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const MyDocument = () => (
+  <Html lang={AppConfig.locale}>
+    <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
 export default MyDocument;
